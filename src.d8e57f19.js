8e4c8b4e06f99cb79f65044e85379fd34053a065
KravchenkoIvan <92682534+KravchenkoIvan@users.noreply.github.com>
2021-12-25 10:08:56 +0000

    Deploying to gh-pages from @ KravchenkoIvan/parsell@99ee6d399d6a1a9d309c14e1be8c2ecc08fa32ae 🚀

--- src.d8e57f19.js
+++ src.d8e57f19.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./../images/demo.jpg":[["demo.090c7c6d.jpg","nKR3"],"nKR3"],"./../images/rectangle.jpg":[["rectangle.67b0a58f.jpg","FuOI"],"FuOI"]}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/parsell/src.8d5d8ab7.js.map
+//# sourceMappingURL=/parsell/src.d8e57f19.js.map
